d56560a17ce861c33a2aa0153b4c6480c25f2f82
Valerii <valera_belousov@ukr.net>
2023-03-28 16:55:38 +0300

    28.03.16:55

--- server.php
+++ server.php
@@ -5,17 +5,18 @@ foreach ($_POST as $key => $data) {
 }
 $uploads_dir = 'files';
 foreach ($_FILES as $file) {
-    if ($_FILES['error'] !== 0) {
-        echo 'error';
+    if ($file['error'] !== 0) {
+        exit('Error uploading file');
         $filename = $file['name'];
         $tmp_name = $file['tmp_name'];
     }
-}
-if (!is_dir($uploads_dir)) {
-    mkdir($uploads_dir, 0700);
-    move_uploaded_file($tmp_name, "$uploads_dir/$filename");
-} else {
-    move_uploaded_file($tmp_name, "$uploads_dir/$filename");
+
+    if (!is_dir($uploads_dir)) {
+        mkdir($uploads_dir, 0700);
+        move_uploaded_file($tmp_name, "$uploads_dir/$filename");
+    } else {
+        move_uploaded_file($tmp_name, "$uploads_dir/$filename");
+    }
 }
 echo "<pre>";
 print_r($newArray);
